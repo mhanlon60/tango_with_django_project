515f654c53fc125c1fb3d646237e12e166af9af4
mhanlon60 <mbhanlon@sky.com>
2022-04-01 10:15:14 +0100

    Finished Chapter 8 with all tests passing

--- rango/forms.py
+++ rango/forms.py
@@ -26,9 +26,9 @@ class PageForm(forms.ModelForm):
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
 
-class Meta:
-    # Provide an association between the ModelForm and a model
-    model = Page
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Page
 
     # What fields do we want to include in our form?
     # This way we don't need every field in the model present.
@@ -37,6 +37,6 @@ class Meta:
     # we can either exclude the category field from the form,
 
 
-exclude = ('category',)
+        exclude = ('category',)
 # or specify the fields to include (don't include the category field).
 # fields = ('title', 'url', 'views')
